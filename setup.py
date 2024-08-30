c813d7dd5ae2cf775e8cec912dbafb3f86ee811a
chungnh <chungnguyenhuu911@gmail.com>
2024-08-30 14:54:05 +0700

    setup source

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='tradingview_ta',
-    version='3.3.0',
+    name='tradingview_ta_v2',
+    version='1.0.0',
     description="Unofficial TradingView technical analysis API wrapper.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/brian-the-dev/python-tradingview-ta',
-    author='Brian',
-    author_email='brian@brianthe.dev',
-    packages=['tradingview_ta'],
+    url='https://github.com/codingfordummies911/tradingview-ta-v2',
+    author='codingfordummies911',
+    author_email='codingfordummies911@gmail.com',
+    packages=['tradingview_ta_v2'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -25,6 +25,6 @@ setuptools.setup(
     project_urls={
         'Demo': 'https://tradingview.brianthe.dev',
         'Documentation': 'https://python-tradingview-ta.readthedocs.io',
-        'Source': 'https://github.com/brian-the-dev/python-tradingview-ta',
+        'Source': 'https://github.com/codingfordummies911/tradingview-ta-v2',
     },
 )
